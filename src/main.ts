c82f77c388ad2c814a055eabf4738d2d5b2953a8
Decio Neto <deciom.neto10@gmail.com>
2023-04-20 12:45:45 -0300

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import "@angular/compiler";
+import "zone.js";
+
+import "./style.css";
+
+import { bootstrapApplication } from "@angular/platform-browser";
+
+import { AppComponent } from "~/app.component";
+import { appRouting } from "~/routes/app.routing";
+
+bootstrapApplication(AppComponent, { providers: [...appRouting] });
---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { splitVendorChunkPlugin, defineConfig } from "vite";
+import { dependencies } from "./package.json";
+
+import path from "path";
+
+function renderChunks(deps) {
+  let chunks = {};
+  Object.keys(deps).forEach((key) => {
+    if ([].includes(key)) return;
+    chunks[key] = [key];
+  });
+  return chunks;
+}
+
+export default defineConfig({
+  plugins: [splitVendorChunkPlugin()],
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: [],
+          ...renderChunks(dependencies),
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./src/app"),
+    },
+  },
+});
