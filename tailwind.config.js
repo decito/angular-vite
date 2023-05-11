35ab5a60cc3efdfcf59578d36592c519ce0de58d
Decio Neto <deciom.neto10@gmail.com>
2023-05-11 16:54:23 -0300

    chore: add global css and tailwind variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,16 @@
-/** @type {import('tailwindcss').Config} */ module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
-  plugins: [],
-};
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'rgb(var(--color-primary) / <alpha-value>)',
+        secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+        success: 'rgb(var(--color-success) / <alpha-value>)',
+        warning: 'rgb(var(--color-warning) / <alpha-value>)',
+        danger: 'rgb(var(--color-danger) / <alpha-value>)',
+        disabled: 'rgb(var(--color-disabled) / <alpha-value>)'
+      }
+    }
+  }
+}
